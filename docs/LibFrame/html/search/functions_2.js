ac9f3e038ec560402b37a8dd090b4818eb59fd23
dalambert <daniel.lambert@clermont.in2p3.fr>
2018-06-19 16:16:13 +0200

    remove doc and docs

---
+++ docs/LibFrame/html/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['extractheader',['ExtractHeader',['../classDecodeFrame.html#a7d3cb26d923ec22430687186a002c1a8',1,'DecodeFrame']]]
+];
---
+++ docs/LibFrame/html/search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "bcdefgimnprstv~",
+  1: "ds",
+  2: "bdrt",
+  3: "cdefgimnprs~",
+  4: "e",
+  5: "cdv",
+  6: "d"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "defines",
+  6: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Macros",
+  6: "Pages"
+};
+
